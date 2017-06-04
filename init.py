2ea1246f43e9b56d86d26d05c2665f6869f4b7c0
Anton Atanasov <atanassov1337@gmail.com>
2017-06-04 22:50:59 +0200

    added file iteration and storing into list

--- init.py
+++ init.py
@@ -1,26 +1,51 @@
 import math
 import os.path
 from itertools import repeat
+import glob
+import re
+
 
 """
 Array used to store all the images for 10-fold cross validation
 """
 ten_fold_array = [[] for i in repeat(None, 10)]
 sub_folders_list = []
+folder_list = []
+main_dir = "20news-18828"
+files_list = []
 
-def iterate_class_folders(number_of_classes):
-    main_dir = "101_ObjectCategories"
-    # sub_folders_list = []
-    all_folder_names = os.listdir(main_dir)
-    temp_folders = ['ant', 'accordion']
-    # all_folder_names = temp_folders
-    # all_folder_names.remove(".DS_Store")
 
-    for tempIndex, _ in enumerate(range(number_of_classes)):
-        choice_item_index = random.randrange(len(all_folder_names))
-        choice_item = all_folder_names.pop(choice_item_index - 1)
-        sub_folders_list.append(choice_item)
+def iterate_class_folders():
+    all_folders = os.listdir(main_dir)
+    return all_folders
 
-    for folder in sub_folders_list:
-        retrieve_image_from_folder(folder)
+    # for folder in main_dir:
+    #     folder_list.append(folder)
+
+
+def get_words(text):
+    return re.compile('\w+').findall(text)
+
+
+def extract_words(folder):
+    for infile in glob.glob(os.path.join('{0}/{1}'.format(main_dir, folder), '*')):
+        review_file = open(infile, 'r').read()
+        words = get_words(review_file)
+        files_list.append(words)
+
+folder_list = iterate_class_folders()
+
+
+for folder in folder_list:
+    extract_words(folder)
+
+
+
+
+
+end = 1
+
+
+    # for folder in sub_folders_list:
+    #     retrieve_file_from_folder(folder)
 
