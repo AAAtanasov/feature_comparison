72cf523f4b9cfe4ccc5a15466601156beb914e05
Anton Atanasov <atanassov1337@gmail.com>
2017-06-04 22:53:05 +0200

    clean up

--- init.py
+++ init.py
@@ -4,12 +4,6 @@ from itertools import repeat
 import glob
 import re
 
-
-"""
-Array used to store all the images for 10-fold cross validation
-"""
-ten_fold_array = [[] for i in repeat(None, 10)]
-sub_folders_list = []
 folder_list = []
 main_dir = "20news-18828"
 files_list = []
@@ -18,9 +12,6 @@ files_list = []
 def iterate_class_folders():
     all_folders = os.listdir(main_dir)
     return all_folders
-
-    # for folder in main_dir:
-    #     folder_list.append(folder)
 
 
 def get_words(text):
@@ -33,19 +24,11 @@ def extract_words(folder):
         words = get_words(review_file)
         files_list.append(words)
 
-folder_list = iterate_class_folders()
 
+folder_list = iterate_class_folders()
 
 for folder in folder_list:
     extract_words(folder)
 
 
-
-
-
 end = 1
-
-
-    # for folder in sub_folders_list:
-    #     retrieve_file_from_folder(folder)
-
